5ac74ea9891bf92ef9c3157fa2cc821173811a3d
Andrew Helmer <ahelmer7@live.com>
2018-07-29 23:51:37 -0700

    comments put in

--- client/scripts/views/admin-view.js
+++ client/scripts/views/admin-view.js
@@ -12,7 +12,7 @@ var app = app || {};
       event.preventDefault();
       let token = event.target.passphrase.value;
 
-      // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      // COMMENT: Is the token cleared out of local storage? yes Do you agree or disagree with this structure? yes
       $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/admin`, {token})
         .then(res => {
           if(res) {
